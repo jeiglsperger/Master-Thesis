206fd927881939fdb18335866029277be4268fce
Zepp3 <36689688+Zepp3@users.noreply.github.com>
2021-11-05 15:17:33 +0100

    Update CalendarIngest.py

--- DataIngestSchachtschneider/CalendarIngest.py
+++ DataIngestSchachtschneider/CalendarIngest.py
@@ -12,9 +12,8 @@ def add_public_holidays(df: pd.DataFrame, federal_state: str):
     :param federal_state: federal_state for retrieving data
     :return: Dataframe with public holidays added
     """
-    federal_state_holidays = holidays.CountryHoliday(country='Germany', prov=federal_state, years=[2001, 2002, 2003, 2004, 2005, 2006, 2007,
-                                                                                                   2008, 2009, 2010, 2011, 2012, 2013, 2014,
-                                                                                                   2015, 2016, 2017, 2018, 2019, 2020])
+    federal_state_holidays = holidays.CountryHoliday(country='Germany', prov=federal_state, years=[2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 
+                                                                                                   2014, 2015, 2016, 2017, 2018, 2019, 2020])
     if 'public_holiday' not in df.columns:
         df['public_holiday'] = 'no'
     for date in df.index:
