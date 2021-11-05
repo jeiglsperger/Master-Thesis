ac688fa2da728d28f641ef3253acda0ac2d8c926
Zepp3 <36689688+Zepp3@users.noreply.github.com>
2021-11-05 15:14:02 +0100

    Add files via upload

---
+++ DataIngestSchachtschneider/CalendarIngest.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import datetime
+import holidays
+
+import ferien
+
+
+def add_public_holidays(df: pd.DataFrame, federal_state: str):
+    """
+    Add public holidays in specific federal state
+    :param df: DataFrame where data should be added
+    :param federal_state: federal_state for retrieving data
+    :return: Dataframe with public holidays added
+    """
+    federal_state_holidays = holidays.CountryHoliday(country='Germany', prov=federal_state, years=[2001, 2002, 2003, 2004, 2005, 2006, 2007,
+                                                                                                   2008, 2009, 2010, 2011, 2012, 2013, 2014,
+                                                                                                   2015, 2016, 2017, 2018, 2019, 2020])
+    if 'public_holiday' not in df.columns:
+        df['public_holiday'] = 'no'
+    for date in df.index:
+        if date in federal_state_holidays:
+            df.at[date, 'public_holiday'] = federal_state_holidays[date]
+    
+    return df
+
+
+def add_school_holidays(df: pd.DataFrame, federal_state: str):
+    """
+    Add school holidays in specific federal state
+    :param df: DataFrame where data should be added
+    :param federal_state: federal_state for retrieving data
+    :return: Dataframe with school holidays added
+    """
+    for year in range(2001, 2021):
+        federal_state_school_holidays = ferien.state_vacations(state_code=federal_state, year=year)
+        if 'school_holiday' not in df.columns:
+            df['school_holiday'] = 'no'
+        index = df['Auf. Datum']
+        for date in index:
+            for vac in federal_state_school_holidays:
+                if vac.start.date() <= date <= vac.end.date():
+                    df.at[date, 'school_holiday'] = vac.name
+                
+    return df
+    
---
+++ DataIngestSchachtschneider/add_externals.py
@@ -0,0 +1,55 @@
+import pandas as pd
+from geopy.geocoders import Nominatim
+from tqdm import tqdm
+import IngestHelper, WeatherIngest, CalendarIngest
+from sklearn.impute import SimpleImputer
+import numpy as np
+
+    
+def add_externals():
+    """
+    Take data, add external data and save
+    """
+    geolocator = Nominatim(user_agent='plantgrid', timeout=3)
+    location = geolocator.geocode('Schulweg 23, 26203 Wardenburg')
+    federal_state = 'NI'
+    
+    
+    df = pd.DataFrame(pd.read_csv('Schachtschneider_further.csv', header=0))
+    
+    
+    print("----- Adding external data -----")
+    df['Auf. Datum'] = pd.to_datetime(df['Auf. Datum'])
+    df['Auf. Datum'] = df['Auf. Datum'].dt.date
+    df.index = df['Auf. Datum']
+    
+    
+    df = WeatherIngest.add_daily_weather_data(df=df, location=location)
+    df = CalendarIngest.add_public_holidays(df=df, federal_state=federal_state)
+    df = CalendarIngest.add_school_holidays(df=df, federal_state=federal_state)
+    
+    
+    df['Auf. Datum'] = pd.to_datetime(df['Auf. Datum'])
+    df['weekday'] = df['Auf. Datum'].dt.dayofweek
+    df = df.drop("Auf. Datum", axis=1)
+    
+    
+    for index, row in df.iterrows():
+        cumsum = 0
+        for i in range(783):
+            cumsum += row[i]
+        if cumsum == 0 and row["weekday"] != 0 and row["public_holiday"] == "no":
+            for i in range(783):
+                df.at[index, df.columns[i]] = np.nan
+                
+                
+    imputer = SimpleImputer(missing_values=np.NaN, strategy="most_frequent")
+    imputed_df = pd.DataFrame(imputer.fit_transform(df))
+    imputed_df.columns = df.columns
+    imputed_df.index = df.index        
+    imputed_df.to_csv('Schachtschneider_externals.csv')
+    
+    
+if __name__ == '__main__':
+
+    add_externals()
