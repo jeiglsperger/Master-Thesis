d092bc3047fc1fe001e8a6ccbed767389a44591d
Zepp3 <36689688+Zepp3@users.noreply.github.com>
2021-11-06 21:58:21 +0100

    Update evaluate_schachtschneider.py

--- timeGAN/evaluate_schachtschneider.py
+++ timeGAN/evaluate_schachtschneider.py
@@ -5,7 +5,7 @@ from sdv.evaluation import evaluate
 
 def test_samples(database_name, print_data=False):
 
-    test, fake = load_data('/JOsef/TimeGAN/test_data_schachtschneider.csv', '/JOsef/TimeGAN/fake_data_' + database_name + '.csv')
+    test, fake = load_data('test_data_schachtschneider.csv', 'fake_data_' + database_name + '.csv')
     
     with pd.option_context('display.max_columns', None):
         if print_data:
@@ -15,8 +15,8 @@ def test_samples(database_name, print_data=False):
         if print_data:
             print(test)
             
-    cat_cols = ["total_prec_flag", "public_holiday", "school_holiday", "weekday", "quarter"]
+    cat_cols = ["public_holiday", "weekday"]
     table_evaluator = TableEvaluator(test, fake, cat_cols=cat_cols)
     table_evaluator.visual_evaluation()
-    table_evaluator.evaluate(target_col='CutFlowers', target_type='regr')
+    table_evaluator.evaluate(target_col='Lavandula', target_type='regr')
     print(evaluate(fake, test, aggregate=False))
