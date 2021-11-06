48960ded075663bf3ab403b1004731f51c271c8c
Zepp3 <36689688+Zepp3@users.noreply.github.com>
2021-11-06 21:56:35 +0100

    Add files via upload

---
+++ timeGAN/evaluate_cashierdata.py
@@ -0,0 +1,22 @@
+from table_evaluator import load_data, TableEvaluator
+import pandas as pd
+from sdv.evaluation import evaluate
+
+
+def test_samples(database_name, print_data=False):
+
+    test, fake = load_data('/JOsef/TimeGAN/test_data.csv', '/JOsef/TimeGAN/fake_data_' + database_name + '.csv')
+    
+    with pd.option_context('display.max_columns', None):
+        if print_data:
+            print(fake)
+
+    with pd.option_context('display.max_columns', None):
+        if print_data:
+            print(test)
+            
+    cat_cols = ["total_prec_flag", "public_holiday", "school_holiday", "weekday", "quarter"]
+    table_evaluator = TableEvaluator(test, fake, cat_cols=cat_cols)
+    table_evaluator.visual_evaluation()
+    table_evaluator.evaluate(target_col='CutFlowers', target_type='regr')
+    print(evaluate(fake, test, aggregate=False))
