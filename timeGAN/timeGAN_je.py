2e797a0457b6206e74d3c399fe50d35029de99fb
Zepp3 <36689688+Zepp3@users.noreply.github.com>
2022-01-22 16:03:22 +0100

    Update timeGAN_je.py

--- timeGAN/timeGAN_je.py
+++ timeGAN/timeGAN_je.py
@@ -294,10 +294,10 @@ def objective(trial, dataset_train, dataset_val, num_samples, columns, num_colum
     # hyperparameters
     module = trial.suggest_categorical("module", ["gru", "lstm", "lstmLN"])
     hidden_dim = trial.suggest_categorical("hidden_dim", [6, 12, 24])
-    batch_size = trial.suggest_categorical("batch_size", [4, 8, 16])
+    batch_size = trial.suggest_categorical("batch_size", [2, 4, 8])
     num_layer = trial.suggest_int("num_layer", 3, 6, 1)
     iterations = trial.suggest_categorical("iterations", [1, 10, 100])
-    seq_len = trial.suggest_categorical("seq_len", [1, 5, 20])
+    seq_len = trial.suggest_categorical("seq_len", [5, 10, 20])
     
 
     data_cut = cut_data(dataset_train, seq_len)
